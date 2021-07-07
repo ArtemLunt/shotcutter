a949113706fcd207565fdb509ec407d72e6dd0ad
Artem Ivanov <aivanov@smartrecruiters.com>
2021-07-07 18:59:33 +0200

    refactor(handling): add the error dto

--- identity/src/main/java/com/shotcutter/identity/api/v1/advice/UserDataExceptionHandlerAdvice.java
+++ identity/src/main/java/com/shotcutter/identity/api/v1/advice/UserDataExceptionHandlerAdvice.java
@@ -1,14 +1,12 @@
 package com.shotcutter.identity.api.v1.advice;
 
 import com.shotcutter.identity.user.InvalidUserDataException;
+import com.shotcutter.identity.user.InvalidUserDataExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.Collections;
-import java.util.Map;
 
 @ControllerAdvice
 public class UserDataExceptionHandlerAdvice {
@@ -16,8 +14,8 @@ public class UserDataExceptionHandlerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidUserDataException.class)
     @ResponseBody
-    Map<String, String> onUserDataInvalid(InvalidUserDataException exception) {
-        return Collections.singletonMap("message", exception.getMessage());
+    InvalidUserDataExceptionDTO onUserDataInvalid(InvalidUserDataException exception) {
+        return InvalidUserDataExceptionDTO.fromException(exception);
     }
 
 }
