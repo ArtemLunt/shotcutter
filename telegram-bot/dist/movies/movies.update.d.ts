36dbd7317c4a1926341ea4122efceed587620219
Artem Ivanov <33367503+ArtemLunt@users.noreply.github.com>
2022-06-17 01:48:56 +0300

    chore(global): remove sources (#12)

--- telegram-bot/dist/movies/movies.update.d.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from 'telegraf';
-export declare class MoviesUpdate {
-    private readonly _moviesService;
-    onText(ctx: Context): void;
-    private _getPreformattedGenresString;
-}
